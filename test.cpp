288e9648c283bd5f52531b2ab72396bf132029a3
celvins <vard-94@mail.ru>
2014-10-02 09:48:35 +0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,11 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
 #include <iostream>
 #include <math.h>
 #include <time.h>
@@ -5,12 +13,10 @@
 #define M 100
 using namespace std;
 
-double population[M][N], fitness[M];
-
-double fun(int i){
+double fun(double **population, int i){
 	return population[i][0] - population[i][1] + pow(population[i][2], 2);
 }
-void puzirek(){
+void puzirek(double * fitness){
 	int i, kol = 1;
 	double temp;
 	while(kol > 0){
@@ -26,43 +32,46 @@ void puzirek(){
 	}
 }
 int main() {
-	int i, j, k, b, x;
+	double **population = new double*[N];
+	double *fitness = new double[M];
+	for(int i = 0; i < M; i++)
+		population[i] = new double[M];
 	double sum = 0, roulette[M][2];
 	srand(time(0));
 	//Начальная популяция
-	for(i = 0; i < M; i++){
-		for(j = 0; j < N; j++)
+	for(int i = 0; i < M; i++){
+		for(int j = 0; j < N; j++)
 			population[i][j] = -200.15 / 15 + rand() % 401;
 	}
 
 	//10 поколений
-	for(j = 0; j < 10; j++){
+	for(int j = 0; j < 10; j++){
 		printf("Поколение %d\n", j + 1);
-		for(i = 0; i < M; i++)
+		for(int i = 0; i < M; i++)
 			printf("%lf %lf %lf\n", population[i][0], population[i][1], population[i][2]);
 
 		//Значения функции
-		for(i = 0; i < M; i++){
-			fitness[i] = fun(i);
+		for(int i = 0; i < M; i++){
+			fitness[i] = fun(population, i);
 			sum += fitness[i];
 		}
 		/*
 		 * Сортировка fitness
 		 */
-		puzirek();
+		puzirek(fitness);
 
 		//Рулетка
-		for(i = 0; i < M; i++){
+		for(int i = 0; i < M; i++){
 			roulette[i][0] = fitness[i];
 			roulette[i][1] = fitness[i + 1];
 //			printf("%5lf %5lf\n", roulette[i][0], roulette[i][1]);
 		}
 		//Новая популяция
-		for(k = 0; k < M; k++){
-			x = rand() % int(sum);
-			for(i = 0; i < M; i++){
+		for(int k = 0; k < M; k++){
+			int x = rand() % int(sum);
+			for(int i = 0; i < M; i++){
 				if((x >= roulette[i][0]) && (x < roulette[i][1]))
-					for(b = 0; b < N; b++)
+					for(int b = 0; b < N; b++)
 						population[k][b] = population[i][b];
 			}
 		}
