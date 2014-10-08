41cf5f3150f7599ee635f8ad53b1f425593e59ea
celvins <vard-94@mail.ru>
2014-10-08 13:37:42 +0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,14 +9,14 @@
 #include <iostream>
 #include <math.h>
 #include <time.h>
-#define N 2
+#define N 3
 #define M 100
 using namespace std;
 
-double fun(double **population, int i){
-	return population[i][0] - population[i][1] + pow(population[i][2], 2);
+double fun(float * population){
+	return population[0] - population[1] + pow(population[2], 2);
 }
-void puzirek(double * fitness){
+void puzirek(float * fitness){
 	int i, kol = 1;
 	double temp;
 	while(kol > 0){
@@ -32,27 +32,29 @@ void puzirek(double * fitness){
 	}
 }
 int main() {
-	double **population = new double*[N];
-	double *fitness = new double[M];
+	int x, percent;
+	float roulette[M][2], sum, mutation;
+
+	float **population = new float*[M];
 	for(int i = 0; i < M; i++)
-		population[i] = new double[M];
-	double sum = 0, roulette[M][2];
+		population[i] = new float[N];
+
+	float *fitness = new float[M];
 	srand(time(0));
+
 	//Начальная популяция
-	for(int i = 0; i < M; i++){
+	for(int i = 0; i < M; i++)
 		for(int j = 0; j < N; j++)
 			population[i][j] = -200.15 / 15 + rand() % 401;
-	}
 
 	//10 поколений
 	for(int j = 0; j < 10; j++){
+		sum = 0;
 		printf("Поколение %d\n", j + 1);
-		for(int i = 0; i < M; i++)
-			printf("%lf %lf %lf\n", population[i][0], population[i][1], population[i][2]);
 
 		//Значения функции
 		for(int i = 0; i < M; i++){
-			fitness[i] = fun(population, i);
+			fitness[i] = fun(population[i]);
 			sum += fitness[i];
 		}
 		/*
@@ -68,16 +70,38 @@ int main() {
 		}
 		//Новая популяция
 		for(int k = 0; k < M; k++){
-			int x = rand() % int(sum);
+			x = rand() % int(sum);
 			for(int i = 0; i < M; i++){
-				if((x >= roulette[i][0]) && (x < roulette[i][1]))
-					for(int b = 0; b < N; b++)
+				if((x >= roulette[i][0]) && (x < roulette[i][1])){
+					for(int b = 0; b < N; b++){
+						percent = rand() % 100; //Вероятность мутации
+						if((percent > 0) && (percent <= 20)){
+							cout << "mutatiya" << endl;
+							mutation = rand() % 5; //Мутация
+							if((rand() % 2) == 1){
+								cout << "\n" << population[i][b] << "\n";
+								population[i][b] += (population[i][b] * (mutation / 100));
+								cout << "+ " << mutation << " " << population[i][b] << endl;
+							}
+							else{
+								population[i][b] -= (population[i][b] * (mutation / 100));
+								cout << "- " << mutation << endl;
+							}
+						}
 						population[k][b] = population[i][b];
+					}
+				}
 			}
+		}
+		for(int i = 0; i < M; i++){
+			for(int k = 0; k < N; k++)
+				printf("%5lf ", population[i][k]);
+			cout << endl;
 		}
 	}
 	for(int i = 0; i < M; i++)
 		delete []population[i];
 	delete []population;
+	delete []fitness;
 	return EXIT_SUCCESS;
 }
