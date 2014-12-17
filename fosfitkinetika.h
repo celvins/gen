94572150fc5752929d0249d56492bd43cb21a54b
celvins <vard-94@mail.ru>
2014-12-17 16:13:57 +0300

    Create fosfitkinetika.h

---
+++ fosfitkinetika.h
@@ -0,0 +1,12 @@
+/*
+ * fosfitkinetika.h
+ *
+ *  Created on: 13.12.2014
+ *      Author: Celvins
+ */
+
+#ifndef FOSFITKINETIKA_H_
+#define FOSFITKINETIKA_H_
+
+double F_SUM(double *);
+#endif /* FOSFITKINETIKA_H_ */
